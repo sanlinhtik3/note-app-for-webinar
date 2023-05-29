9cebfa8c8ee9b95d403c345c468ff3f2751cf197
sanlinhtik3 <sanlinhtike@Sans-MacBook-Pro.local>
2023-05-29 20:35:19 +0630

    get note and notes,update and delete

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,10 +1,19 @@
 import Note from "../models/noteModel.js";
 
+// Path             --> http://localhost:9000/api/note
+// Description      --> Read a note
+const getNote = async(req, res) => {
+  const {id} = req.params;
+  const note = await Note.findOne({_id: id});
+  res.json(note);
+};
+
 
 // Path             --> http://localhost:9000/api/note
 // Description      --> Read a note
-const getNotes = (req, res) => {
-  res.json({ message: "Read a Note" });
+const getNotes = async(req, res) => {
+  const notes = await Note.find()
+  res.json(notes);
 };
 
 // Method           --> POST
@@ -19,14 +28,19 @@ const createNote = async(req, res) => {
 
 // Path             --> http://localhost:9000/api/note
 // Description      --> Update a note
-const updateNote = (req, res) => {
-  res.json({ message: "Create a Note" });
+const updateNote = async(req, res) => {
+  const {id} = req.params;
+  const note = await Note.findByIdAndUpdate(id, req.body, {new: true});
+  res.json(note);
 };
 
 // Path             --> http://localhost:9000/api/note
 // Description      --> Delete a note
-const deleteNote = (req, res) => {
-  res.json({ message: "Delete a Note" });
+const deleteNote = async(req, res) => {
+  const { id } = req.params;
+  // const note = await Note.findByIdAndDelete(id)
+  const note = await Note.deleteOne({_id: id})
+  res.json(note);
 };
 
-export { getNotes, createNote, updateNote, deleteNote };
+export { getNotes,getNote, createNote, updateNote, deleteNote };
--- backend/routes/noteRoute.js
+++ backend/routes/noteRoute.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { createNote, deleteNote, getNotes, updateNote } from "../controllers/noteController.js";
+import { createNote, deleteNote, getNote, getNotes, updateNote } from "../controllers/noteController.js";
 
 const router = express.Router()
 
-
-
 router.get('/', getNotes)
+router.get("/:id", getNote);
 router.post('/', createNote)
 router.put('/:id', updateNote)
 router.delete('/:id', deleteNote)
